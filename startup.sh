a2cbe2c1231d337634f52a29823ce1bc5e4b1698
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-07-30 09:51:51 +0300

    Replace slash delimiters with space I assume space is the safest character to use as a delimiter

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
-sed -i s/AWS_KEY/$AWS_KEY/ /config.yml
-sed -i s/AWS_SECRET/$AWS_SECRET/ /config.yml
-sed -i s/AWS_REGION/$AWS_REGION/ /config.yml
-sed -i s/S3_BUCKET/$S3_BUCKET/ /config.yml
+sed -i "s AWS_KEY $AWS_KEY "  /config.yml
+sed -i "s AWS_SECRET $AWS_SECRET "  /config.yml
+sed -i "s AWS_REGION $AWS_REGION "  /config.yml
+sed -i "s S3_BUCKET $S3_BUCKET "  /config.yml
 
 cat /config.yml
 
