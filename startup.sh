73fa1f60ffdb9cd33dc35c462ea0a6a87d8b931a
Martin Garton <martin.garton@ft.com>
2015-08-06 13:19:11 +0100

    don't write credentials to stdout.

--- startup.sh
+++ startup.sh
@@ -3,6 +3,4 @@ sed -i "s AWS_SECRET $AWS_SECRET " /config.yml
 sed -i "s AWS_REGION $AWS_REGION " /config.yml
 sed -i "s S3_BUCKET $S3_BUCKET " /config.yml
 
-cat /config.yml
-
 registry /config.yml
